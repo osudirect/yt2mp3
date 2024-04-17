75bcc1e7975c7029efb79f9562d1e78f461f865f
Nanoo <45440100+Calemy@users.noreply.github.com>
2024-04-17 02:23:18 +0200

    qol: rewrite
    this should be correct

--- index.js
+++ index.js
@@ -8,34 +8,34 @@ let videoID = 0;
 const fastify = f({ trustProxy: true })
 if(!fs.existsSync("temp")) fs.mkdirSync("temp")
 fastify.get("/:id", async ({ params }, reply) => {
-    const { videoDetails } = await ytdl.getBasicInfo(params.id).catch(undefined)
+    const { videoDetails, formats } = await ytdl.getInfo(params.id).catch(undefined)
     if(!videoDetails) return reply.code(404).send({ error: "Invalid id" })
-    const vID = videoID
-    videoID++
-    ytdl(params.id, {
-        quality: "highestaudio",
-        filter: "audioonly"
-    }).pipe(fs.createWriteStream(`temp/${vID}`)).on("finish", () => {
-        console.log(`Finished downloading ${videoDetails.title} - ${vID}`)
-        const ffmpeg = spawn("ffmpeg", ["-i", `temp/${vID}`, "-vn", `temp/${vID}.mp3`])
-        ffmpeg.on("close", () => {
-            console.log(`Finished converting ${videoDetails.title} - ${vID}`)
-            fs.rmSync(`temp/${vID}`, { recursive: true, force: true })
-            const buffer = fs.statSync(`temp/${vID}.mp3`).size
-            reply.header('Content-Length', buffer);
-            reply.header("Content-Disposition", `attachment; filename="${encodeURI(videoDetails.title)}.mp3"`)
-            const stream = fs.createReadStream(`temp/${vID}.mp3`)
-            stream.on("end", () => {
-                fs.rmSync(`temp/${vID}.mp3`, { recursive: true, force: true })
-            })
-            reply.send(stream)
-        })
-    }).on("error", (e) => {
-        console.error(e, params.id, vID)
-        reply.code(500).send({ error: `Something went wrong while downloading`})
+    const length = videoDetails.lengthSeconds >> 0
+    if(length > (process.env.maxLength || 600)) return reply.code(400).send({ error: `Audio too long (${length}/${process.env.maxLength || 600} seconds)`})
+
+    const format = ytdl.chooseFormat(formats, {
+        filter: "audioonly",
+        quality: "highestaudio"
     })
+
+    const vID = videoID++
+    console.log(format)
+    console.log(Math.floor(format.averageBitrate / 1000))
+    const ffmpeg = spawn('ffmpeg', ['-i', 'pipe:', '-vn', '-b:a', `128k`, '-f', 'mp3', '-'])
+    ffmpeg.on("exit", (code) => {
+        if(code != 0) return;
+        console.log(`Finished converting ${videoDetails.title} - ${vID}`)
+    })
+    ffmpeg.on("spawn", () => {
+        console.log(`Started converting ${videoDetails.title} - ${vID}`)
+        reply.header("Content-Disposition", `attachment; filename="${encodeURI(videoDetails.title)}.mp3"`)
+        reply.header("Content-Length", (128000 / 8) * (format.approxDurationMs / 1000).toString())
+        reply.send(ffmpeg.stdout)
+        ytdl(params.id, { format }).pipe(ffmpeg.stdin).on("error", () => console.log(`Aborted converting ${videoDetails.title} - ${vID}`))
+    })
+
     return reply;
 })
 
-fastify.listen({ host: "0.0.0.0", port: process.env.port })
-console.log(`Server running on port ${process.env.port}`)
+fastify.listen({ host: "0.0.0.0", port: process.env.port || 2461 })
+console.log(`Server running on port ${process.env.port || 2461}`)
